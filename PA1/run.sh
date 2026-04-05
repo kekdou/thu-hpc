d40f93661dc8aaa3068c25058a5ce4fd3c9d5ec5
kekdou <298746435@qq.com>
2026-04-05 22:35:53 +0800

    optimize sort

--- PA1/run.sh
+++ PA1/run.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-# run on 1 machine * 28 process, feel free to change it!
-srun -N 1 -n 28 $*
+num_elements=$2
 
+if [ "$num_elements" -le 100 ]; then
+    N=1
+    n=1
+elif [ "$num_elements" -le 1000 ]; then
+    N=1
+    n=2
+elif [ "$num_elements" -le 10000 ]; then
+    N=1
+    n=9
+elif [ "$num_elements" -le 100000 ]; then
+    N=2
+    n=56
+elif [ "$num_elements" -le 1000000 ]; then
+    N=2
+    n=52
+elif [ "$num_elements" -le 10000000 ]; then
+    N=2
+    n=40
+else
+    N=2
+    n=32
+fi
+
+# run on 1 machine * 28 process, feel free to change it!
+srun -N $N -n $n --cpu-bind sockets --exclusive $*
